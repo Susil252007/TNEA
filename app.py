1c86f5f6478ac8838d6213f12240102bd2ead01d
Susil252007 <susil.kumar.031125@gmail.com>
2025-07-23 23:02:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -240,8 +240,8 @@ st.markdown(
     The data used in this app is collected from publicly available sources provided by TNEA.<br>
     This app is not affiliated with or endorsed by TNEA or the Directorate of Technical Education (DoTE), Tamil Nadu.<br><br>
 
-    <strong>Contact</strong>: +91-9789937399<br>
-    <strong>Email</strong>: iamsusil@gmail.com<br>
+    <strong>Contact</strong>: +91-9789937399 <br>
+    <strong>Email</strong>: iamsusil@gmail.com <br>
     <strong>Developed by</strong>: R.Susil Kumar <br>
     &copy; 2025 <strong>TNEA Info App</strong>. All rights reserved.
     </div>
